344e78914dbac77d021022fa63666b6ba54ddd1f
Sinu Thomas <sinu@bb-system-1272.mynet>
2019-08-29 21:37:25 +0100

    changed js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello... Edit <code>src/App.js</code> and save to reload.
+          Hello... Changed in feature branch..Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
